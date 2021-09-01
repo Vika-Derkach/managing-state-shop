6a4c7acc1b400bd4a96f413a113b22345356ad1e
Vika <queenviktoriyphp@gmail.com>
2021-09-01 12:11:42 +0300

    details about product

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
 export default function Detail() {
-  return <h1>Detail</h1>;
-
+  const { id } = useParams();
+  const { data: product, error, loading } = useFetch("products/" + id);
+  //   return <h1>Detail</h1>;
+  if (error) throw error;
+  if (loading) return <Spinner />;
   // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 }
