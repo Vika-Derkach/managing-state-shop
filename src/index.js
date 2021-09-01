6aeb9f957b4dea8e58c27c4afe3490b014835933
Vika <queenviktoriyphp@gmail.com>
2021-09-01 11:40:51 +0300

    react-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
-
 ReactDOM.render(
   <ErrorBoundary>
-    <App />
+    <Router>
+      {" "}
+      <App />
+    </Router>
   </ErrorBoundary>,
   document.getElementById("root")
 );
