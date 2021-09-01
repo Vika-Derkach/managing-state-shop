bb13e8f7b8713b5291a7b80410027d80843899e6
Vika <queenviktoriyphp@gmail.com>
2021-09-01 10:54:13 +0300

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import { getProducts } from "./services/productService";
+import Spinner from "./Spinner";
 export default function App() {
   const [size, setSize] = useState("");
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     getProducts("shoes")
       .then((response) => setProducts(response))
-      .catch((e) => setError(e));
+      .catch((e) => setError(e))
+      .finally(() => setLoading(false));
   }, []);
 
   function renderProduct(p) {
@@ -28,6 +32,7 @@ export default function App() {
     ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
     : products;
   if (error) throw error;
+  if (loading) return <Spinner />;
   return (
     <>
       <div className="content">
