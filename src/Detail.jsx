6dab6ba4c3e82d6502c0722b16ef5634cd5e85ef
Vika <queenviktoriyphp@gmail.com>
2021-09-01 13:05:02 +0300

    creating a shoe sizeselect

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 
 export default function Detail() {
+  const [sku, setSku] = useState("");
   const { id } = useParams();
   const navigate = useNavigate();
   const { data: product, error, loading } = useFetch("products/" + id);
@@ -13,11 +14,22 @@ export default function Detail() {
   if (!product) return <PageNotFound />;
   if (error) throw error;
   // TODO: Display these products details
+
   return (
     <div id="detail">
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <select id="size" value={sku} onChange={(e) => setSku(e.target.value)}>
+        <option value="">What size?</option>
+        {product.skus.map((s) => {
+          return (
+            <option key={s.sku} value={s.sku}>
+              {s.size}
+            </option>
+          );
+        })}
+      </select>
       <p>
         <button className="btn btn-primary" onClick={() => navigate("/cart")}>
           Add to Cart
