0d950729d84d2a6e7393b01bf4f86f2dd0d457b4
Vika <queenviktoriyphp@gmail.com>
2021-09-01 12:42:14 +0300

    navigation

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
+
 export default function Detail() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { data: product, error, loading } = useFetch("products/" + id);
-  //   return <h1>Detail</h1>;
-  if (error) throw error;
+
   if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
+  if (error) throw error;
   // TODO: Display these products details
   return (
     <div id="detail">
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <p>
+        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+          Add to Cart
+        </button>
+      </p>
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
