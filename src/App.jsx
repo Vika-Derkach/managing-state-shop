4b77a025476847e2f475f3b61ee5080e82a2b827
Vika <queenviktoriyphp@gmail.com>
2021-09-01 16:28:50 +0300

    put data in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Cart from "./Cart";
@@ -7,8 +7,17 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
 export default function App() {
-  const [cart, setCart] = useState([]);
-  const [count, setcount] = useState(1);
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? [];
+    } catch {
+      console.error("The cart could not be parsed into JSON.");
+      return [];
+    }
+  });
+
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+
   function addToCart(id, sku) {
     setCart((items) => {
       const itemInCart = items.find((i) => i.sku === sku);
@@ -23,6 +32,7 @@ export default function App() {
       }
     });
   }
+
   function updateQuantity(sku, quantity) {
     console.log(sku, quantity);
     //   setcount(quantity);
--- src/Cart.jsx
+++ src/Cart.jsx
@@ -47,10 +47,17 @@ export default function Cart({ cart, updateQuantity }) {
 
   if (loading) return <Spinner />;
   if (error) throw error;
+  const numItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <section id="cart">
-      <h1>Cart</h1>
+      <h1>
+        {numItemsInCart === 0
+          ? "No Items in the Cart"
+          : `${numItemsInCart} Item${
+              numItemsInCart > 1 ? "s" : ""
+            } in my Cart`}{" "}
+      </h1>
       <ul>{cart.map(renderItem)}</ul>
     </section>
   );
